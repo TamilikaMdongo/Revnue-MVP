9d959503634a660c84bc19b798e873ddbe983602
Tamilika Mdongo <mdongotamilika45@gmail.com>
2026-09-16 21:44:27 +0200

    read for pilot testing

--- Client/app/src/pages/Home.jsx
+++ Client/app/src/pages/Home.jsx
@@ -4,7 +4,38 @@ import DashCard from '../components/DashCard'
 import data from '../components/Cards.json'
 import Dashboard from '../components/Dashboard'
 import RecentTransactions from '../components/RecentTransactions'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+
 const Home = () => {
+  const user = {
+    first_name:''
+  } 
+const [userData, setUserData] = useState({
+  first_name: ""
+});
+const userId = localStorage.getItem('user_id')
+useEffect(() => {
+   
+  const getUser = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/users/profile/${userId}`
+      );
+
+      setUserData({
+        first_name: response.data.first_name
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getUser();
+}, [userId]);
+
   return (
      <div className="home">
       <Sidebar/>
@@ -12,7 +43,7 @@ const Home = () => {
         <div className="header">
           <div><span className="eyebrow">Overview</span><h1>Dashboard</h1></div>
           <div className="header-child">
-            <div className="user-copy"><strong>John Doe</strong><span>Personal account</span></div>
+            <div className="user-copy"><strong>{userData.first_name}</strong><span>Personal account</span></div>
             <div className="circle-avatar" aria-label="John Doe profile picture"></div>
           </div>
         </div>
